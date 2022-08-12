767df01c45f2e633c133432534b83a8633a1f46d
Galih Juansaputra <galih.juansaputra@gmail.com>
2022-08-13 07:57:51 +0800

    mencoba dom

---
+++ js/script.js
@@ -0,0 +1,10 @@
+const dataBuku = document.getElementById('dataBuku');
+console.log(dataBuku);
+
+const popUp = document.getElementById('popUp');
+console.log(popUp);
+
+const mark = document.getElementById('tandai');
+console.log(mark);
+// mark.innerText = "Memanipulasi Data";
+
