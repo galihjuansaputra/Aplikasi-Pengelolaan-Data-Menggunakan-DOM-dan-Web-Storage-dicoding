a092456c8429fef07888573f0a93c3d950cb44ee
Galih Juansaputra <galih.juansaputra@gmail.com>
2022-08-16 16:00:48 +0800

    menyelesaikan fungsi dan menerapkan local storage

--- js/script.js
+++ js/script.js
@@ -1,61 +1,225 @@
-// mendapatkan element popup msg modal
-const popUp = document.getElementById('popUp');
+document.addEventListener('DOMContentLoaded', function () {
+  const submitFormBuku = document.getElementById('newBookData');
+  submitFormBuku.addEventListener('submit', function (event) {
+    event.preventDefault();
+    addBook();
+  });
+  loadDataFromStorage();
+});
 
-// function delete
-function deleteDataBuku() {
-  popUp.removeAttribute('hidden');
+// 
+const bukus = [];
+const RENDER_EVENT = 'render-buku';
+
+// tambah data buku
+function addBook() {
+  const generatedID = generateId();
+  const textJudul = document.getElementById('judulBuku').value;
+  const textPenulis = document.getElementById('penulisBuku').value;
+  const intTahun = document.getElementById('tahunBuku').value;
+  const tandaBuku = document.getElementById('markBuku').checked;
+
+  const bookObject = generateDataBuku(generatedID, textJudul, textPenulis, intTahun, tandaBuku);
+  bukus.push(bookObject);
+ 
+  document.dispatchEvent(new Event(RENDER_EVENT));
+  saveData();
 }
-document.getElementById('delete').onclick = deleteDataBuku;
 
-// function cancel delete
-function noDelete() {
-  popUp.setAttribute('hidden',true);  
+// fungsi generate id
+function generateId() {
+  return +new Date();
 }
-document.getElementById('tidak').onclick = noDelete;
 
-// function yes delete
-function yesDelete() {
-  popUp.setAttribute('hidden',true);
+function generateDataBuku(id,title,author,year,isCompleted){
+  return {
+    id,
+    title,
+    author,
+    year,
+    isCompleted
+  }
 }
-document.getElementById('ya').addEventListener('click', yesDelete);
 
+document.addEventListener(RENDER_EVENT, function () {
+  
+  const uncompletedBUKUList = document.getElementById('bukus');
+  uncompletedBUKUList.innerHTML = '';
+ 
+  const completedBUKUList = document.getElementById('completed-bukus');
+  completedBUKUList.innerHTML = '';
 
-// DIATAS KODE ADALAH KODE JS, BELAJARNYA DI BAWAH VVVV
-
-// Penggunaan Custom Event
-
-// window.addEventListener('load', function() {
-//   const tombol = document.getElementById('tombol');
-//   tombol.addEventListener('changeCaption', customEventHandler);
-//   tombol.addEventListener('click', function() {});
-// });
-
-// function customEventHandler(ev) {
-//   console.log('Event ' + ev.type + ' telah dijalankan');
-//   const caption = document.getElementById('caption');
-//   caption.innerText = 'Anda telah membangkitkan custom event';
-// }
-
-// tombol.addEventListener('click', function () {
-//   tombol.dispatchEvent(changeCaption);
-// });
-
-
-const dataBuku = document.getElementById('dataBuku');
-console.log(dataBuku);
-
-const mark = document.getElementById('tandai');
-console.log(mark);
-// mark.innerText = "Memanipulasi Data";
-
-
-
-let objBuku = {
-    id: 3657848524,
-    title: 'Harry Potter and the Philosopher\'s Stone',
-    author: 'J.K Rowling',
-    year: 1997,
-    isComplete: false,
+  for (const bookItem of bukus) {
+    const bukuElement = makeBuku(bookItem);
+    if (!bookItem.isCompleted)
+      uncompletedBUKUList.append(bukuElement);
+    else
+      completedBUKUList.append(bukuElement);
+    
   }
 
-  console.log(objBuku);
+});
+
+
+function makeBuku(bookObject) {
+
+  const bukuTitle = document.createElement('h2');
+  bukuTitle.innerText = bookObject.title;
+
+  const bukuAuthor = document.createElement('p');
+  bukuAuthor.innerText = `Penulis: ${bookObject.author}`;
+
+  const bukuYear = document.createElement('p');
+  bukuYear.innerText = `Tahun: ${bookObject.year}`;
+
+
+  const textContainer = document.createElement('div');
+  textContainer.classList.add('book');
+  textContainer.append(bukuTitle, bukuAuthor, bukuYear);
+  textContainer.setAttribute('id', `buku-${bookObject.id}`);
+
+
+  if (bookObject.isCompleted) {
+    const undoButton = document.createElement('button');
+    undoButton.classList.add('mark');
+    undoButton.innerText = 'Tandai Belum Selesai Dibaca';
+ 
+    undoButton.addEventListener('click', function () {
+      undoTaskFromCompleted(bookObject.id);
+    });
+ 
+    const trashButton = document.createElement('button');
+    trashButton.classList.add('delete');
+    trashButton.innerText = 'Hapus Buku';
+ 
+    trashButton.addEventListener('click', function () {
+      removeTaskFromCompleted(bookObject.id);
+    });
+ 
+    textContainer.append(undoButton, trashButton);
+
+  } else {
+    const checkButton = document.createElement('button');
+    checkButton.classList.add('mark');
+    checkButton.innerText = 'Tandai Selesai Dibaca';
+    
+    checkButton.addEventListener('click', function () {
+      addTaskToCompleted(bookObject.id);
+    });
+
+    const trashButton = document.createElement('button');
+    trashButton.classList.add('delete');
+    trashButton.innerText = 'Hapus Buku';
+ 
+    trashButton.addEventListener('click', function () {
+      removeTaskFromCompleted(bookObject.id);
+    });
+    
+    textContainer.append(checkButton, trashButton);
+  }
+
+  return textContainer;
+
+}
+
+function addTaskToCompleted (bookId) {
+  const bookTarget = findBook(bookId);
+ 
+  if (bookTarget == null) return;
+ 
+  bookTarget.isCompleted = true;
+  document.dispatchEvent(new Event(RENDER_EVENT));
+  saveData();
+}
+
+function findBook(bookId) {
+  for (const bookItem of bukus) {
+    if (bookItem.id === bookId) {
+      return bookItem;
+    }
+  }
+  return null;
+}
+
+function removeTaskFromCompleted(bookId) {
+  const bukuTarget = findBookIndex(bookId);
+ 
+  if (bukuTarget === -1) return;
+ 
+  bukus.splice(bukuTarget, 1);
+  document.dispatchEvent(new Event(RENDER_EVENT));
+  saveData();
+}
+ 
+ 
+function undoTaskFromCompleted(bukuId) {
+  const bukuTarget = findBook(bukuId);
+ 
+  if (bukuTarget == null) return;
+ 
+  bukuTarget.isCompleted = false;
+  document.dispatchEvent(new Event(RENDER_EVENT));
+  saveData();
+}
+
+function findBookIndex(bookId) {
+  for (const index in bukus) {
+    if (bukus[index].id === bookId) {
+      return index;
+    }
+  }
+ 
+  return -1;
+}
+
+
+function saveData() {
+    const parsed = JSON.stringify(bukus);
+    localStorage.setItem(STORAGE_KEY, parsed);
+    document.dispatchEvent(new Event(SAVED_EVENT));
+}
+
+const SAVED_EVENT = 'saved-buku';
+const STORAGE_KEY = 'BOOKSHELF_APPS';
+
+document.addEventListener(SAVED_EVENT, function () {
+  console.log(localStorage.getItem(STORAGE_KEY));
+});
+
+
+function loadDataFromStorage() {
+  const serializedData = localStorage.getItem(STORAGE_KEY);
+  let data = JSON.parse(serializedData);
+ 
+  if (data !== null) {
+    for (const buku of data) {
+      bukus.push(buku);
+    }
+  }
+ 
+  document.dispatchEvent(new Event(RENDER_EVENT));
+}
+
+
+// // mendapatkan element popup msg modal
+
+// const popUp = document.getElementById('popUp');
+
+// // function delete
+// function deleteDataBuku() {
+//   popUp.removeAttribute('hidden');
+// }
+// document.getElementById('delete').addEventListener('click', deleteDataBuku);
+
+// // function cancel delete
+// function noDelete() {
+//   popUp.setAttribute('hidden',true);  
+// }
+// document.getElementById('tidak').onclick = noDelete;
+
+// // function yes delete
+// function yesDelete() {
+//   popUp.setAttribute('hidden',true);
+// }
+// document.getElementById('ya').addEventListener('click', yesDelete);
+
